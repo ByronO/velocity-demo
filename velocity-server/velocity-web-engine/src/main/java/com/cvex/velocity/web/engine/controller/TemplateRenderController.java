e050692feb375ae353ae470cd2fd04db3a1f8cc6
Bryan Acuna Nunez <nayrban@users.noreply.github.com>
2022-07-18 11:08:06 -0600

    Update TemplateRenderController.java

--- velocity-server/velocity-web-engine/src/main/java/com/cvex/velocity/web/engine/controller/TemplateRenderController.java
+++ velocity-server/velocity-web-engine/src/main/java/com/cvex/velocity/web/engine/controller/TemplateRenderController.java
@@ -1,6 +1,7 @@
 package com.cvex.velocity.web.engine.controller;
 
 import com.cvex.velocity.web.engine.domain.TemplateConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,17 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
+@Slf4j
 @Controller
 @RequestMapping("/templates")
 public class TemplateRenderController {
 
-    @GetMapping(value = {"{template}/{view}"})
-    public ModelAndView renderView(@PathVariable(value = "template") String template, @PathVariable("view") String view, Model model, HttpSession session) {
+    @GetMapping(value = {"{template}/{view}/{occurrence}"})
+    public ModelAndView renderView(@PathVariable(value = "template") String template, @PathVariable("view") String view,
+                                   @PathVariable(value = "occurrence", required = false) String occurrence, Model model,
+                                   HttpSession session) {
+        log.info("Loading occurrence for session %s".formatted(occurrence));
+
         String templatePath = "%s/%s".formatted(template, view);
 
         final TemplateConfig templateConfig = (TemplateConfig) session.getAttribute(TemplateConfigurationController.SESSION_CONFIG);
